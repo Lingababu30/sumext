d7ad0500f4fc4de9a7ec76310dc1efffe5680d6c
Lingababu30 <lingababuaithagoni30@gmail.com>
2025-12-01 10:49:01 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 class simpjava{
     public void sum(){
         int a=30;
-        int b=29;
+        int b=24;
         System.out.println(a+b);
     }
 }
@@ -9,6 +9,7 @@ class Sum{
     public static void main(String args[]){
         simpjava s=new simpjava();
         System.out.println("The sum of two numbers:");
+        sytem.out.println("sum after updating");
         s.sum();
     }
 }
